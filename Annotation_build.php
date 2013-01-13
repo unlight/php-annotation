f2c3595e22576080fd8114f9b3b0ee2ae4698c45
iamthes <s91630277@gmail.com>
2013-01-14 02:19:21 +0400

    Losted argument

--- Annotation_build.php
+++ Annotation_build.php
@@ -2,7 +2,7 @@
 
 final class Annotation_build {
 
-	public static function build() {
+	public static function build($reflector) {
 	    static $_Annotation_Parser;
 	    if (is_null($_Annotation_Parser)) {
 	        $_Annotation_Parser = new AnnotationParser();
